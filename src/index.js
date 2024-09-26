b1b203fdc33f12be74ca336c98a981a23655a950
Dmytro Artiuk <dimairak88@wp.pl>
2024-09-27 00:11:29 +0200

    add component ThemeSwitch

--- src/index.js
+++ src/index.js
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './GlobalStyle';
-import { theme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <GlobalStyle />
+    <App />
   </React.StrictMode>
 );
 
