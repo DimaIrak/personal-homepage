686d09951e9eef864cba43ccfe87eb0862afb755
Dmytro Artiuk <dimairak88@wp.pl>
2024-10-18 09:16:52 +0200

    Add MainInformation index.js

---
+++ src/features/personalHomepage/PersonalHomepage/Maininformation/index.js
@@ -0,0 +1,20 @@
+import dmytroArtiukProfile from "./profile.jpg";
+import { Avatar, StyledButtonLink, ButtonIcon, Name, Summary, ThisIs, Wrapper, } from "./styled";
+import { email } from "../email";
+
+export const MainInformation = () => (
+    <Wrapper>
+        <Avatar src={dmytroArtiukProfile} alt="Dmytro Artiuk" />
+        <div>
+            <ThisIs>This is</ThisIs>
+            <Name>Dmytro Artiuk</Name>
+            <Summary>
+                I'm a passionate Frontend Developer in love with React,currently looking for new job
+            </Summary>
+            <StyledButtonLink href={`mailto:${email}`} title={email}>
+                <ButtonIcon />
+                Hire me
+            </StyledButtonLink>
+        </div>
+    </Wrapper>
+);
